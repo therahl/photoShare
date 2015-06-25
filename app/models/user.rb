a63ecd0dba2aba1033e986c46cac3dad7e1f1687
therahl <skydropsblue@gmail.com>
2015-06-25 16:43:50 -0700

    removed user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
-  before_save { self.username = username.downcase }
+  # before_save { self.email = email.downcase }
+  # before_save { self.username = username.downcase }
   
   has_many :albums
   has_many :comments
 
-  validates_presence_of :family_name, :password, :given_name, :username, :email, length: { maximum: 50 }
-  validates :email,
-            format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
-            uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, length: { minimum: 6 }
-
+  # validates_presence_of :family_name, :password, :given_name, :username, :email, length: { maximum: 50 }
+  # validates :email,
+  #           format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
+  #           uniqueness: { case_sensitive: false }
+  # validates :password, length: { minimum: 6 }
+  #
 end
--- db/migrate/20150623205628_add_index_to_users_email.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToUsersEmail < ActiveRecord::Migration
-  def change
-    add_index :users, :email, unique: true
-    add_index :users, :username, unique: true
-  end
-end
