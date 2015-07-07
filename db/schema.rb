08a3d99cc8d5f85f7f191abaa8885fb3e4e8b4bc
Jay Hennessy <JAYHENNESSY@GMAIL.COM>
2015-07-07 13:32:20 -0700

    removed sessions

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20150625192036) do
     t.string   "family_name"
     t.string   "username"
     t.string   "email"
-    t.string   "password_digest"
     t.string   "remember_token"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
@@ -50,8 +49,6 @@ ActiveRecord::Schema.define(version: 20150625192036) do
     t.datetime "avatar_updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
